c46cd99a7876b7906f1253bffd1f0c60a0f04bf0
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-05-28 00:47:09 +0500

    Revert "Revert "Plan A Trip Updates""
    This reverts commit c9a966aae42dbdfcff631dba3e396dab86c7ed2a.

--- Controllers/TripPlannerDestinationController.js
+++ Controllers/TripPlannerDestinationController.js
@@ -2,13 +2,15 @@ const TripPlannerDestinationModel = require('../Models/TripPlannerDestination');
 const HttpError = require('../Models/HttpError');
 
 const createTripPlannerDestination = async (req,res,next)=>{
-  const {name} = req.body
+  const {name,north_coordinate,east_coordinate} = req.body
   let newDest
   let dest = await TripPlannerDestinationModel.find({name: name })
   if (dest.length===0){
     try {
       newDest = TripPlannerDestinationModel()
       newDest.name=name
+      newDest.north_coordinate=north_coordinate
+      newDest.east_coordinate=east_coordinate
       newDest.save()
     } catch (error) {
       const err = new HttpError('Creating TripPlannerDestination Failed',500);
@@ -53,6 +55,22 @@ const getTripPlannerDestionationById = async (req,res,next)=>{
   res.send(destinations)
 }
 
+const getTripPlannerDestionationByCoordinates = async (req,res,next)=>{
+  
+  const {to,from} = req.body
+  try {
+     destinations = await TripPlannerDestinationModel.find({"north_coordinate":{$gte:from},"north_coordinate":{$lte:to}})
+  } catch (error) {
+    const err = new HttpError('Getting Trip Planner Destination Faile, please try again',500);
+    return next(err);
+  }
+  if (!destinations) {
+    const error = new HttpError('Could not find any Trip Planner Destination',404);
+    return next(error);
+  }
+  res.send(destinations)
+}
+
 const deleteTripPlannerDestionationById = async (req,res,next)=>{
   let destinations
   let id = req.params.id
@@ -74,3 +92,4 @@ module.exports.createTripPlannerDestination = createTripPlannerDestination
 module.exports.getAllTripPlannerDestionations  = getAllTripPlannerDestionations
 module.exports.getTripPlannerDestionationById  = getTripPlannerDestionationById
 module.exports.deleteTripPlannerDestionationById  = deleteTripPlannerDestionationById
+module.exports.getTripPlannerDestionationByCoordinates  = getTripPlannerDestionationByCoordinates
