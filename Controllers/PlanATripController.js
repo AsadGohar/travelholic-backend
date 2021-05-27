fa42d1306d4fddc3ef2469fc46051ccd5dcda6a1
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-05-28 03:56:46 +0500

    Updated PlanATrip controller
    plan a trip API complete

--- Controllers/PlanATripController.js
+++ Controllers/PlanATripController.js
@@ -16,54 +16,52 @@ const getTripPlanEstimate = async (req,res,next)=>{
     let chkroute = await RouteModel.find({'destination_from':destinations[index],'destination_to':destinations[nextDest]})
     if (chkroute.length===0){
 
-      let to = await TripPlannerDestinationModel.findById(destinations[index])
-      let from = await TripPlannerDestinationModel.findById(destinations[nextDest])
-      const error = new HttpError(`Could not Find Route between ${to.name} to ${from.name}`,404);
+      let from = await TripPlannerDestinationModel.findById(destinations[index])
+      let to = await TripPlannerDestinationModel.findById(destinations[nextDest])
+      const error = new HttpError(`Could not Find Route between ${from.name} to ${to.name}`,404);
       return next(error);
     }
     else {
-      let transport = await TransportModel.find({route:chkroute._id}).sort('-fare');
+      console.log('route id',chkroute)
+      let transport = await TransportModel.find({'route':chkroute[0]._id}).sort('-fare');
       if (transport.length===0){
-        let to = await TripPlannerDestinationModel.findById(destinations[index])
-      let from = await TripPlannerDestinationModel.findById(destinations[nextDest])
-      const error = new HttpError(`No Transport Available between ${to.name} to ${from.name}`,404);
+        let from = await TripPlannerDestinationModel.findById(destinations[index])
+      let to = await TripPlannerDestinationModel.findById(destinations[nextDest])
+      const error = new HttpError(`Could not Find Transport between ${from.name} to ${to.name}`,404);
       return next(error);
       }
       else {
         transports.push(transport)
         minTransportFare+=transport[transport.length-1].fare
         maxTransportFare+=transport[0].fare
+        nextDest++
       }
     }
   }
     
-  // //interating destination array sent from client to find hotels in each destination
-  // for (let index = 0; index < destinations.length; index++) {
-  //   //aggreagation framwork used to find maximum and minimum hotel rent from each destination
-  //   const aggregate =  [
-  //     { $match : { "destination": new mongoose.Types.ObjectId(destinations[index])} } ,
-  //     {
-  //       $group: { _id: "$destination", HotelMin: { $min: "$budget_rent" },HotelMax:{ $max: "$luxury_rent" }}
-  //     },  
-  //   ]
+  //interating destination array sent from client to find hotels in each destination
+  for (let index = 1; index < destinations.length; index++) {
+    //aggreagation framwork used to find maximum and minimum hotel rent from each destination
+    const aggregate =  [
+      { $match : { "destination": new mongoose.Types.ObjectId(destinations[index])} } ,
+      {
+        $group: { _id: "$destination", HotelMin: { $min: "$budget_rent" },HotelMax:{ $max: "$luxury_rent"}}
+      },  
+    ]
 
-  //   //returns an array of object containing minimum and maximum values
-  //   const hotel = await HotelModel.aggregate(aggregate).exec()
-  //   // console.log(hotel)
+    //returns an array of object containing minimum and maximum values
+    const hotel = await HotelModel.aggregate(aggregate).exec()
+    console.log(hotel)
 
-  //   //adding minimum and maximum hotel rent from each destination to the variables
-  //   minHotel +=  hotel[0].HotelMin
-  //   maxHotel +=  hotel[0].HotelMax
-  // }
-  // //old logic to calculate total fare by iterating the fares array and adding each value
-  // fares.forEach(fare=> totalFare+=fare)
-  // //old logic to calculate min and max estimates
-  // minEstimate = minHotel+totalFare
-  // maxEstimate = maxHotel+totalFare
+    //adding minimum and maximum hotel rent from each destination to the variables
+    minHotel +=  hotel[0].HotelMin
+    maxHotel +=  hotel[0].HotelMax
+  }
 
-  // //calculating the new min and max estimate
-  // let newMinEstimate = minHotel+minTransportFare
-  // let newMaxEstimate = maxHotel+maxTransportFare
-  // res.send({newMaxEstimate,newMinEstimate})
+  let newMinEstimate = minHotel+minTransportFare
+  let newMaxEstimate = maxHotel+maxTransportFare
+  res.send(
+    {minTransportFare,maxTransportFare,minHotel,maxHotel,newMaxEstimate,newMinEstimate}
+  )
 }
 module.exports.getTripPlanEstimate  = getTripPlanEstimate
