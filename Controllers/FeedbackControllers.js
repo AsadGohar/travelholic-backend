d3d3886c93762315803a8de3ac30409c3636cc4a
AsadGohar <53337734+AsadGohar@users.noreply.github.com>
2021-07-06 19:19:03 +0500

    email updates in user controller and feedback

--- Controllers/FeedbackControllers.js
+++ Controllers/FeedbackControllers.js
@@ -22,33 +22,46 @@ const createFeedback = async (req,res,next)=>{
     return next(error);
   }
 
-  var mail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'fa17-bse-137@cuilahore.edu.pk',
-      pass: 'FA17BSE137'
-    },
-  tls: {
-    rejectUnauthorized: false
-}
-  });
 
   var mailOptions = {
-    from: 'Travelogic',
+    from: 'info@travelogic.pk',
     to: email,
     subject: 'Feedback Response',
     text: 'Sent!',
     html: output,
   }
 
-  mail.sendMail(mailOptions, function(error, info){
+  let mail = nodemailer.createTransport({
+    host: "travelogic.pk",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "info@travelogic.pk", // generated ethereal user
+      pass: "Travelogic@2021", // generated ethereal password
+    },
+    tls: {
+        rejectUnauthorized: false
+      }
+  });
+
+  mail.verify(function(error, success) {
     if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent With Attachment: ' + info.response);
+      console.log();
+      res.send('err msg > '+ error.message, 'err stk'+error.stack,'err name'+error.name,error)
+    } 
+    else {
+      console.log("Server is ready to take our messages");
+      mail.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error.message);
+          res.send('err msg > '+ error.message, 'err stk'+error.stack,'err name'+error.name,error)
+        } else {
+          console.log('Email sent With Attachment: ' + info.response);
+          res.send({'Mail Sent':email,"Feedback":feedback})
+        }
+      });
     }
   });
-  res.send(feedback)
 }
 //GET ALL FEEDBACK
 const getFeedback = async (req,res,next)=>{
--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -97,33 +97,66 @@ const createUser = async (req, res, next) => {
   else {
     let token = jwt.sign({name,email,password,mobile_num},process.env.JWT_EMAIL_CONFIRMATION_SECRET,{expiresIn:'1h'})
     const output = EmailConfirmation(name,token)
-    var mail = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'fa17-bse-137@cuilahore.edu.pk',
-        pass: 'FA17BSE137'
-      },
-    tls: {
-      rejectUnauthorized: false
-    }
-    });
+
+
+    // var mail = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     user: 'fa17-bse-137@cuilahore.edu.pk',
+    //     pass: 'FA17BSE137'
+    //   },
+      // tls: {
+      //   rejectUnauthorized: false
+      // }
+    // });
   
     var mailOptions = {
-      from: 'Travelogic',
+      from: 'info@travelogic.pk',
       to: email,
       subject: 'Email Confirmation',
       text: 'Sent!',
       html: output,
     }
   
-    mail.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent With Attachment: ' + info.response);
-      }
-    });
-    res.send('Mail Sent')
+    try {
+      
+      let mail = nodemailer.createTransport({
+        host: "travelogic.pk",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "info@travelogic.pk", // generated ethereal user
+          pass: "Travelogic@2021", // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+          }
+      });
+
+      mail.verify(function(error, success) {
+        if (error) {
+          console.log();
+          res.send('err msg > '+ error.message, 'err stk'+error.stack,'err name'+error.name,error)
+        } else {
+          console.log("Server is ready to take our messages");
+          mail.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error.message);
+              res.send('err msg > '+ error.message, 'err stk'+error.stack,'err name'+error.name,error)
+            } else {
+              console.log('Email sent With Attachment: ' + info.response);
+              res.send('Mail Sent'  + info.response,info.envelope,info.messageId)
+            }
+          });
+        }
+      });
+
+     
+
+    } catch (err) {
+      const error = new HttpError('You Are Not Registered', 500);
+      return next(error);
+    }
   }
 }
 
@@ -209,7 +242,7 @@ const updateUserById = async (req, res, next) => {
   const { name, mobile_num, gender } = req.body
   let user
   try {
-    user = await UserModel.findByIdAndUpdate(id, { name, gender, mobile_num })
+    user = await UserModel.findByIdAndUpdate(id, { name, gender, mobile_num },{new:true})
   } catch (err) {
     const error = new HttpError('Finding User Failed', 500);
     return next(error);
